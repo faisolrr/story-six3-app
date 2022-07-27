3a74c736d5d22a2111c1ee9d80dacd3f37f9fa1a
evialf <vifia.rales@gmail.com>
2022-07-27 19:37:08 +0700

    2nd changes feature login

--- src/components/Register/RegisButton.jsx
+++ src/components/Register/RegisButton.jsx
@@ -3,10 +3,7 @@ import React from "react";
 function RegisButton() {
   return (
     <button
-      //   id={id}
       className={`w-full hover:bg-blue-400 bg-white hover:text-white text-blue-400 font-bold my-5 py-2 px-4 rounded-full border-blue-400 border-solid border-2`}
-      //   onClick={onClick}
-      //   disabled={loading}
     >
       Sign up
     </button>
