9844132ac6b1dd1ea58c00a9ff05c6696f045961
evialf <vifia.rales@gmail.com>
2022-07-27 19:37:08 +0700

    2nd changes feature login

--- src/components/Login/LoginInput.jsx
+++ src/components/Login/LoginInput.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useForm } from "@mantine/form";
 
 import { TextInput, PasswordInput } from "@mantine/core";
-// import Login from "../../pages/Login";
 import LoginButton from "./LoginButton";
 import RegisButton from "../Register/RegisButton";
 
@@ -18,11 +17,10 @@ function LoginInput() {
     },
   });
   return (
-    // <div className="grid grid-col m-5 shadow-lg">
     <form onSubmit={form.onSubmit(console.log)}>
       <TextInput
         className="m-5"
-        // mt="sm"
+        mt="sm"
         placeholder="Email"
         {...form.getInputProps("email")}
       />
@@ -35,34 +33,16 @@ function LoginInput() {
         className="text-blue-400 bg-white hover:text-white hover:bg-blue-400"
         id="btn-login"
         label="Log in"
-        //   loading={loading || disabled}
       />
-      <h5 className="text-center text-blue-400">Or</h5>
+      <h5 className="text-center text-blue-400 font-poppins">Or</h5>
       <RegisButton
         className="text-blue-400 bg-white hover:text-white hover:bg-blue-400"
         id="btn-login"
         label="Sign up"
         to="/signup"
       />
-      {/* <Button type="submit" mt="sm">
-          Log in
-        </Button> */}
     </form>
-    // </div>
   );
 }
-// function LoginInput({ id, type, placeholder, value, onChange, disabled }) {
-//   return (
-//     <input
-//       id={id}
-//       type={type}
-//       placeholder={placeholder}
-//       value={value}
-//       onChange={onChange}
-//       disabled={disabled}
-//       defaultValue={value}
-//       className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal"
-//     />
-//   );
-// }
+
 export default LoginInput;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import Header from "../../components/Header";
-// import { useEffect, useState } from "react";
-// import { useNavigate, Link } from "react-router-dom";
-
-import LoginGrid from "../../components/Login/LoginGrid";
+import { SimpleGrid } from "@mantine/core";
+import LoginInput from "../../components/Login/LoginInput";
 
 function Login() {
   return (
     <>
       <Header />
-      <div className="grid grid-row lg:grid-col m-5 p-5 shadow-2xl rounded-2xl">
-        <div className="m-5 p-5 rounded-sm drop-shadow-lg">
-          <LoginGrid />
-        </div>
+      <div className="grid grid-row lg:grid-col mt-7 m-5 p-5 shadow-2xl rounded-2xl">
+        <SimpleGrid
+          breakpoints={[
+            { minWidth: "sm", cols: 1 },
+            { minWidth: "lg", cols: 2 },
+          ]}
+        >
+          <div>
+            <h1 className="text-center font-bold text-blue-400 space-x-4 text-2xl">
+              <span className="space-x-2 font-montserrat text-bold">L</span>
+              <span className="space-x-2 font-montserrat text-bold">O</span>
+              <span className="space-x-2 font-montserrat text-bold">G</span>
+              <span className="space-x-2 font-montserrat text-bold">I</span>
+              <span className="space-x-2 font-montserrat text-bold">N</span>
+            </h1>
+            <LoginInput />
+          </div>
+          <div className="bg-blue-400 text-center text-white sm:text-3xl">
+            <h1 className="m-10 p-10 font-montserrat italic ">WELCOME TO</h1>
+            <h1 className="font-montserrat text-white text-lg lg:text-3xl">
+              <span className="italic">STOR</span>
+              <span className="font-extralight">Y</span>
+              <span className="italic">SiX</span>
+              <span className="font-extrabold">3</span>
+            </h1>
+          </div>
+        </SimpleGrid>
       </div>
     </>
   );
