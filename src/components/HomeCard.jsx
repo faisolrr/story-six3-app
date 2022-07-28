81aeee2feaa35ba4600c64a70bff0b045275d260
faisol <faisolrizkiromdon@gmail.com>
2022-07-28 19:52:19 +0700

    change api from tmdb to jsonplaceholder, set responsive, darkmode, fontsize

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,4 +1,4 @@
-import { Menu, Card, Image, Text, Group } from "@mantine/core";
+import { Menu, Card, Image, Text, Group, Button } from "@mantine/core";
 import { Trash } from "tabler-icons-react";
 
 import React from "react";
@@ -7,50 +7,58 @@ function HomeCard2() {
   return (
     <Text
       color="dimmed"
-      className="grid justify-center hover:underline text-xl font-poppins shadow-lg shadow-black p-5 mx-5 my-5 md:mx-10 md:my-10 lg:mx-20 lg:my-20"
+      className="dark:bg-stone-800 grid text-xs md:text-md lg:text-lg justify-center hover:underline font-poppins shadow-lg shadow-black/40 p-5 mx-5 my-5 md:mx-10 md:my-20 lg:mx-20 lg:my-20"
     >
-      <a href="//">What do you think .... ?</a>
+      <a href="//" className="dark:text-white">
+        What do you think .... ?
+      </a>
     </Text>
   );
 }
 
 function HomeCard(data) {
   return (
-    <Card className="shadow-lg shadow-black p-10">
+    <Card className="shadow-lg shadow-black/40 p-10 dark:bg-stone-800">
       <Card.Section>
         <Group position="apart">
-          <Text className="font-roboto font-boldd text-base">
-            {data.user}
-            <Text color="dimmed" className="ml-2 text-base inline-block ">
+          <Text className="text-xs md:text-md lg:text-lg font-roboto font-boldd dark:text-white">
+            Username{data.user}
+            <Text
+              color="dimmed"
+              className="text-xs md:text-md lg:text-lg ml-2 inline-block dark:text-white"
+            >
               {data.date}h
             </Text>
           </Text>
 
-          <Menu size="xs">
-            <Menu.Item className="hover:bg-stone-100 font-poppins">
+          <Menu size="xs" className="dark:bg-stone-800">
+            <Menu.Item className="font-poppins dark:text-white dark:bg-stone-800">
               Edit
             </Menu.Item>
             <Menu.Item
               color="red"
               icon={<Trash size={14} />}
-              className="hover:bg-stone-100 font-poppins"
+              className="font-poppins dark:bg-stone-800"
             >
               Delete
             </Menu.Item>
           </Menu>
         </Group>
 
-        <Text className="text-justify font-poppins text-base">
+        <Text className="text-xs md:text-md lg:text-lg text-justify font-poppins dark:text-white">
           {data.content}
         </Text>
 
         <Image src={data.picture} className="mt-3" />
+        <Text
+          color="dimmed"
+          className="text-xs md:text-md lg:text-lg hover:underline font-poppins"
+        >
+          <a className="dark:text-white" href="//">
+            View {data.comment} all comments...
+          </a>
+        </Text>
       </Card.Section>
-      <Text color="dimmed" className="hover:underline text-base font-poppins">
-        <a className="" href="//">
-          View {data.comment} all comments...
-        </a>
-      </Text>
     </Card>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,48 +5,49 @@ import axios from "axios";
 
 const HomePage = () => {
   const [userDatas, setUserDatas] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    setLoading(true);
+    // setLoading(true);
     axios
-      .get(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=ccfec1e4c0a0d66091556703ed6a6ee6&language=en-US&page=1`
-      )
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      // .get(`https://myminefield.site/posts`)
       .then((response) => {
-        console.log(response);
-        console.log(response.data.results);
-        const { results } = response.data;
-        console.log(results);
-        if (results) {
-          setUserDatas(results);
+        // console.log(response);
+        // console.log(response.data);
+        const { data } = response;
+        console.log(data);
+        if (data) {
+          setUserDatas(data);
         }
       })
       .catch((error) => {
         alert(error.toString());
-      })
-      .finally(() => setLoading(false));
+      });
+    // .finally(() => setLoading(false));
   };
 
   return (
     <>
       <Header />
-      <HomeCard2 />
-      <div className="grid gap-20 mx-5 my-5 md:mx-10 md:my-10 lg:mx-20 lg:my-20 ">
-        {userDatas.map((userData) => (
-          <HomeCard
-            key={userData.id}
-            user={userData.id}
-            date={userData.vote_average}
-            content={userData.overview}
-            // picture={userData.picture}
-            comment={userData.vote_count}
-          />
-        ))}
+      <div className="dark:bg-black p-1">
+        <HomeCard2 />
+        <div className="grid gap-4 md:gap-20 mx-5 my-5 md:mx-10 md:my-10 lg:mx-20 lg:my-20">
+          {userDatas.map((userData) => (
+            <HomeCard
+              key={userData.id}
+              user={userData.id}
+              date={userData.userId}
+              content={userData.body}
+              // picture={userData.picture}
+              comment={userData.id}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
