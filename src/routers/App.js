2d8ce798d33a1c5035f58bfb6c36d99261a0ace6
faisol <faisolrizkiromdon@gmail.com>
2022-07-29 11:35:50 +0700

    add router for profiles page

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,4 +1,5 @@
 import HomePage from "../pages/HomePage";
+import ProfilesPage from "../pages/ProfilesPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/profiles" element={<ProfilesPage />} />
       </Routes>
     </BrowserRouter>
   );
